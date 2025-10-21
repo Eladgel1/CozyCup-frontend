dfb9491a22cdf63f0bb8ea14956d71689c322181
Elad Gelerenter <eladg3@gmail.com>
2025-10-21 13:34:08 +0300

    Import design tokens

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import './styles/tokens.css';
 import './index.css';
 import { router } from './routes.jsx';
 import { AuthProvider } from '@/features/auth/auth.context.jsx';
